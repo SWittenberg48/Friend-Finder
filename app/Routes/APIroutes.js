8b0ed087cfd7f15fed63360f519f6f03bf996afb
Scott wittenberg <wittenb48@gmail.com>
2019-05-07 09:27:22 -0500

    Changes made to connect and Heroku try

--- app/Routes/APIroutes.js
+++ app/Routes/APIroutes.js
@@ -1,84 +1,74 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var apirouter = express.Router();
+var mysql = require("mysql");
+//THIS IS JUST LOCAL CONNECTION. NEED TO FOLLOW HEROKU/MY SQL INTERFACE
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3307,
+    user: "root",
+    password: "root",
+    database: "friendfinder_db"
+  });
+}
 
-//json for current friends list
-apirouter.get("/api/friends", function(req, res) {
-  res.json(friends);
+// Initiate MySQL Connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    // Selects all of the data from the MySQL profiles table
+    loadProfiles(res);
+  });
+  app.post("/api/friends", function(req, res) {
+    // console.log(req.body);
+    findMatch(req.body, friends);
+  });
+};
+var friends;
+function loadProfiles(response) {
+  connection.query("SELECT * FROM profiles", function(err, result) {
+    if (err) throw err;
 
-//posting a new user and returning a match
-apirouter.post("/api/friends", function(req, res) {
-  console.log("posting...");
-  var newFriend = req.body;
-  console.log(newFriend);
-  //console.log(newFriend['scores[]']); //don't know why scores show up as "scores[]"
-  //console.log(newFriend.scores); //returns scores as 'scores[]'
-  //console.log(parseFloat(JSON.parse(newFriend.scores[0])));//does not work - undefined
+    friends = result;
+    response.json(friends);
+  });
+  console.log(friends);
+}
 
-  //this function coverts users's results into a simple array of numbers
-  var newScore = function(array) {
-    var newScore = [];
-    for (var i = 0; i < array.length; i++) {
-      newScore.push(parseInt(array[i]));
-    }
-    return newScore;
+function findMatch(userData, friends) {
+  console.log(userData);
+  console.log(friends);
+
+  module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+      // Selects all of the data from the MySQL profiles table
+      loadProfiles(res);
+    });
+    app.post("/api/friends", function(req, res) {
+      // console.log(req.body);
+      findMatch(req.body, friends);
+    });
   };
-  //this function calculates difference of elements between two arrays and then sums up the difference
-  var totalDiff = function(arrA, arrB) {
-    delta = 0;
-    for (var i = 0; i < arrA.length; i++) {
-      delta += Math.abs(arrA[i] - arrB[i]);
-    }
-    return delta;
-  };
-  //this function below we find the index of minimum difference
-  //https://stackoverflow.com/questions/11301438/return-index-of-greatest-value-in-an-array
-  function indexOfMin(array) {
-    if (array.length === 0) {
-      return -1;
-    }
+  var friends;
+  function loadProfiles(response) {
+    connection.query("SELECT * FROM profiles", function(err, result) {
+      if (err) throw err;
 
-    var min = array[0];
-    var minIndex = 0;
-
-    for (var i = 1; i < array.length; i++) {
-      if (array[i] < min) {
-        minIndex = i;
-        min = array[i];
-      }
-    }
-
-    return minIndex;
+      friends = result;
+      response.json(friends);
+    });
+    console.log(friends);
   }
 
-  var newFriendScore = newScore(newFriend["scores[]"]); //array of numbers
-  var currentFriendScores = []; //array of arrays
-  var differences = [];
-
-  //recall friends is an array of objects
-  for (var i = 0; i < friends.length; i++) {
-    currentFriendScores.push(newScore(friends[i]["scores[]"]));
+  function findMatch(userData, friends) {
+    console.log(userData);
+    console.log(friends);
   }
-
-  //below we take each friend and find difference between that friend and new friend
-  for (var i = 0; i < currentFriendScores.length; i++) {
-    differences.push(totalDiff(newFriendScore, currentFriendScores[i]));
-  }
-  console.log("calculating...");
-  //console.log(newFriendScore);
-  //console.log(currentFriendScores);
-  //console.log(differences);
-
-  var minFriend = indexOfMin(differences);
-  var matchFriend = friends[minFriend];
-  console.log("matching...");
-  console.log(matchFriend);
-
-  //push to array so this person is not included in above calculation for shortest path
-  friends.push(newFriend);
-  res.json(matchFriend);
-});
-
-//Export API routes for server.js to use.
-module.exports = apirouter;
+}
--- app/Routes/HTMLroutes.js
+++ app/Routes/HTMLroutes.js
@@ -1,17 +1,12 @@
-// Pull in required dependencies
 var path = require("path");
 
-// Export HTML routes
+// Below code handles when users visit  a page.
+// In each of the below cases the user is shown an HTML page of content
 module.exports = function(app) {
-  // console.log('___ENTER htmlRoutes.js___');
-
-  // Home page
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
-  // Survey page
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
